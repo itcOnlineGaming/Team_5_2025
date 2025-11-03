e8288505d15311582218b5034b64f2346c6798bf
Jake Fitzgerald <115044918+Jake-Fitzgerald@users.noreply.github.com>
2025-11-03 12:13:51 +0000

    Page Navigation setup and working

--- Sprint_9/my-app/src/routes/Homescreen/+page.svelte
+++ Sprint_9/my-app/src/routes/Homescreen/+page.svelte
@@ -1,23 +1,18 @@
 <nav>
-
+    
 </nav>
 
 <script lang="ts">
 	import { goto } from "$app/navigation";
-  
 
 
     function OnClickNavigate()
     {
         console.log("Confirm Button Clicked");
         goto("/Homescreen", {noScroll:false});
-
     }
 
 </script>
 
-<h1> Hello, please press continue to go to the Home Screen </h1>
-
-<button on:click={OnClickNavigate}>Confirm</button>
-
+<h1>Home Screen</h1>
 
