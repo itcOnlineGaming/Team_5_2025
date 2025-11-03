d76b8b89c33511e2ad9763925081b516bd763ae5
Jake Fitzgerald <115044918+Jake-Fitzgerald@users.noreply.github.com>
2025-11-03 14:57:56 +0000

    Exit Screen created along with a template table that displays the results from the tests.

--- Sprint_9/my-app/src/routes/+page.svelte
+++ Sprint_9/my-app/src/routes/+page.svelte
@@ -4,20 +4,26 @@
 
 <script lang="ts">
 	import { goto } from "$app/navigation";
-  
-
 
     function OnClickNavigate()
     {
         console.log("Confirm Button Clicked");
         goto("/Homescreen", {noScroll:false});
+    }
 
+        function OnClickNavigateExitScreen()
+    {
+        console.log("Exit Screen Button Clicked");
+        goto("/Exitscreen", {noScroll:false});
     }
 
 </script>
 
+
 <h1> Hello, please press continue to go to the Home Screen </h1>
 
 <button on:click={OnClickNavigate}>Confirm</button>
+
+<button on:click={OnClickNavigateExitScreen}>Exit Screen</button>
 
 
---
+++ Sprint_9/my-app/src/routes/Exitscreen/+page.svelte
@@ -0,0 +1,81 @@
+<!--- Exit Sceen -->
+<script lang="ts">
+	import { goto } from "$app/navigation";
+    // Timer import
+    import { onMount } from 'svelte';
+
+    function OnClickNavigate()
+    {
+        console.log("Confirm Button Clicked");
+        goto("/Homescreen", {noScroll:false});
+
+    }
+
+    // Test results:
+    let session_results = 
+    [
+    { number: 1, description: 'Total Session Time', result: "1000" + " seconds" },
+    { number: 2, description: 'Total Button Clicks', result: "100" + " clicks" },
+    { number: 3, description: 'Mood Inputted', result: "2" },
+    { number: 4, description: 'Incorrect input', result: "5" + " wrong clicks" },
+    { number: 5, description: 'Mood Inputted', result: "2" }
+    ];
+
+</script>
+
+<button on:click={OnClickNavigate}>Confirm</button>
+
+<h2> Result Table </h2>
+
+<table>
+  <thead>
+    <tr>
+      <th>NO.</th>
+      <th>DESCRIPTION</th>
+      <th>RESULT</th>
+    </tr>
+  </thead>
+  <tbody>
+    {#each session_results as session_result}
+      <tr>
+        <td>{session_result.number}</td>
+        <td>{session_result.description}</td>
+        <td>{session_result.result}</td>
+      </tr>
+    {/each}
+  </tbody>
+</table>
+
+<body> 
+    <h1> Goodbye! </h1>
+    
+</body>
+
+
+<style>
+  table 
+  {
+    width: 100%;
+    border-collapse: collapse;
+  }
+  th, td 
+  {
+    border: 1px solid #241414;
+    padding: 0.5rem;
+    text-align: left;
+  }
+  th 
+  {
+    background-color: #aaaaaa;
+  }
+  tr:nth-child(even) 
+  {
+    background-color: #d4d4d4;
+  }
+  body
+  {
+    background-color: rgb(240, 240, 240);
+  }
+  
+</style>
+
