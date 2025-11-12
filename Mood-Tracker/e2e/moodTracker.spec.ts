c888854778cde3421df8c2f25a1ed801ea5f1f7b
Stephen Dunne <C00194821@setu.ie>
2025-11-12 15:26:32 +0000

    updated playwright test to reflect new URLs using

--- Mood-Tracker/e2e/moodTracker.spec.ts
+++ Mood-Tracker/e2e/moodTracker.spec.ts
@@ -1,8 +1,11 @@
 import { test, expect } from '@playwright/test';
 
+const BASE_PATH = '/Team_5';
+const BASE_URL = `http://localhost:5173${BASE_PATH}`;
+
 test('has title', async ({ page }) => {
-  await page.goto('http://localhost:5173/${base}');
-  await expect(page).toHaveURL("http://localhost:5173/${base}");
+  await page.goto(BASE_URL);
+  await expect(page).toHaveURL(BASE_URL);
 
   //Expect a title "to contain" a substring.
   //await expect(page).toHaveTitle();
