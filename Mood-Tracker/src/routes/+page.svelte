c75010d13bd3e33229cd486be7fce32424c7a8b2
Nathan-Byrne <c00281281@setu.ie>
2025-11-24 13:20:37 +0000

    #25 Changed Navigation to Homescreen

--- Mood-Tracker/src/routes/+page.svelte
+++ Mood-Tracker/src/routes/+page.svelte
@@ -1,31 +1,27 @@
 <script lang="ts">
-	import { goto } from "$app/navigation";
-    import { base } from '$app/paths';
+	//import { goto } from "$app/navigation";
+    //import { base } from '$app/paths';
     import PopupStart from "./popupStart.svelte";    
     import './styles.css';
-    import { getAgreementStatus } from './consentAgreement';
+    //import { getAgreementStatus } from './consentAgreement';
 
-    function OnClickNavigate()
-    {
-        console.log("Confirm Button Clicked");
-        if (getAgreementStatus())
-            goto(`${base}/Homescreen`, {noScroll:false});
-        else
-            alert("Agreement has not been accepted. Please accept the agreement to access the application");
-    }
+    // function OnClickNavigate()
+    // {
+    //     console.log("Confirm Button Clicked");
+    //     if (getAgreementStatus())
+    //         goto(`${base}/Homescreen`, {noScroll:false});
+    //     else
+    //         alert("Agreement has not been accepted. Please accept the agreement to access the application");
+    // }
 
-    function OnClickNavigateExitScreen()
-    {
-        console.log("Exit Screen Button Clicked");
-        goto(`${base}/Exitscreen`, {noScroll:false});
-    }
+    // function OnClickNavigateExitScreen()
+    // {
+    //     console.log("Exit Screen Button Clicked");
+    //     goto(`${base}/Exitscreen`, {noScroll:false});
+    // }
 
 </script>
 
-<h1> Hello, please press continue to go to the Home Screen </h1>
-
-<button on:click={OnClickNavigate}>Confirm</button>
-
-<button on:click={OnClickNavigateExitScreen}>Exit Screen</button>
+<h1> Hello, please accept the Agreement to start Testing</h1>
 
 <PopupStart></PopupStart>
