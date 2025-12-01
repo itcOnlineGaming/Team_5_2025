faca5981a7cc34199981ea7daffa124dc92bbea7
Nathan-Byrne <c00281281@setu.ie>
2025-12-01 12:34:11 +0000

    #61 Added Tests for Yoga Page

--- Mood-Tracker/e2e/moodTracker.spec.ts
+++ Mood-Tracker/e2e/moodTracker.spec.ts
@@ -11,6 +11,20 @@ test(`Test on Playwright Page`, async ({ page }) => {
 test('Show Start Screen Popup', async ({ page }) => {
   await page.goto(BASE_URL);
 
-  await page.getByRole('button', {name: 'View Information'}).click();
+  await page.getByRole('button', { name: 'View Information' }).click();
   await expect(page.getByRole('heading', { name: 'Hello, and welcome to our application called "Mood Tracker".' })).toBeVisible();
+});
+
+// Yoga Exercise
+test('Yoga Page: Load Main Menu', async ({ page }) => {
+  await page.goto(BASE_URL + "/YogaScreen");
+
+  await expect(page.getByRole('heading', { name: 'Yoga Exercises' })).toBeVisible();
+});
+
+test('Yoga Page: Submit Feeling', async ({ page}) => {
+  await page.goto(BASE_URL + "/YogaScreen");
+
+  await page.getByRole('button', { name: 'Open Current Feeling Popup' }).click();
+  await expect(page.getByRole('heading', { name: 'How do you feel currently?' })).toBeVisible();
 });
